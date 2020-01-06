ac8bec617cab998c288197692af45edef5b7ef52
Leonardo Moyano <leomoyano7@gmail.com>
2020-01-06 16:29:39 -0300

    Update 2 6/1

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import style from '../Slider/Slider.scss';
+import image1 from '../../images/Slider-images/petshop.png'
+import image2 from '../../images/Slider-images/consultas.png'
+import image3 from '../../images/Slider-images/internacion.png'
+import image4 from '../../images/Slider-images/emergencias.png'
 
 class Slider extends Component {
     render() {
@@ -9,37 +13,38 @@ class Slider extends Component {
                     <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
                     <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
                     <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                    <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
                 </ol>
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(45).jpg" class="d-block w-100" alt="..."/>
+                        <img src={image1} class="d-block w-100" alt="..."/>
                             <div class="carousel-caption d-none d-md-block">
                                 <h3>Petshop</h3>
                                 <p>Ponemos a tu alcance todo lo que tu fiel amigo necesita</p>
                             </div>
                     </div>
                         <div class="carousel-item">
-                            <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(46).jpg" class="d-block w-100" alt="..."/>
+                            <img src={image2} class="d-block w-100" alt="..."/>
                                 <div class="carousel-caption d-none d-md-block">
                                     <h3>Consultas</h3>
                                     <p>Realice sus consultas online</p>
                                 </div>
                         </div>
-                            <div class="carousel-item">
-                                <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(47).jpg" class="d-block w-100" alt="..."/>
+                        <div class="carousel-item">
+                                <img src={image3} class="d-block w-100" alt="..."/>
                                     <div class="carousel-caption d-none d-md-block">
                                         <h3>Internacion/Cirugia</h3>
                                         {/* <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p> */}
                                     </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(47).jpg" class="d-block w-100" alt="..."/>
+                        </div>
+                        <div class="carousel-item">
+                                <img src={image4} class="d-block w-100" alt="..."/>
                                     <div class="carousel-caption d-none d-md-block">
                                         <h3>Emergencias</h3>
                                         <p>Llamá: 0800-234-2345</p>
                                     </div>
-                            </div>
-                            </div>
+                        </div>
+                    </div>
                             <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
                                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                                 <span class="sr-only">Previous</span>
