cadbe65da07f3fde48623b41661a41ff5fb2c766
franco flores <franco210789@gmail.com>
2020-01-17 12:45:27 -0300

    services

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../Components/Header/Navbar/Navbar';
 import Slider from '../Components/Slider/Slider';
+import Services from '../Components/Services/Services';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
       <Slider></Slider>
+      <Services></Services>
     </div>
   );
 }
