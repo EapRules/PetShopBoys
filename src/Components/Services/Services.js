e26da17207d2f99477425ac190c7d7c078483b2b
franco flores <franco210789@gmail.com>
2020-01-17 15:27:06 -0300

    servicios terminados

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -17,11 +17,75 @@ class Services extends Component {
                 <div className="row">
 
                     <div className="col-lg-4 col-sm-12 text-center p-3">
-                        <h3 id="services">Consulta</h3>
-                        <img src={imagen3} class="d-block w-100" alt="..." />
 
+                        <div class="card text-center" style={{ width: '18rem' }}>
+                            <img src={imagen3} class="card-img-top" alt="..."></img>
+                            <div class="card-body">
+                                <h5 class="card-title">Consulta</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Saber +</a>
+                            </div>
+                        </div>
                     </div>
 
+                    <div className="col-lg-4 col-sm-12 text-center p-3">
+                        <div class="card text-center" style={{ width: '18rem' }}>
+                            <img src={imagen7} class="card-img-top" alt="..."></img>
+                            <div class="card-body">
+                                <h5 class="card-title">Estetica</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Saber +</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-4 col-sm-12 text-center p-3">
+                        <div class="card text-center" style={{ width: '18rem' }}>
+                            <img src={imagen6} class="card-img-top" alt="..."></img>
+                            <div class="card-body">
+                                <h5 class="card-title">Cirugia</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Saber +</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-4 col-sm-12 text-center p-3">
+                        <div class="card text-center" style={{ width: '18rem' }}>
+                            <img src={imagen5} class="card-img-top" alt="..."></img>
+                            <div class="card-body">
+                                <h5 class="card-title">Internacion</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Saber +</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-4 col-sm-12 text-center p-3">
+                        <div class="card text-center" style={{ width: '18rem' }}>
+                            <img src={imagen1} class="card-img-top" alt="..."></img>
+                            <div class="card-body">
+                                <h5 class="card-title">Vacunacion</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Saber +</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-4 col-sm-12 text-center p-3">
+                        <div class="card text-center" style={{ width: '18rem' }}>
+                            <img src={imagen4} class="card-img-top" alt="..."></img>
+                            <div class="card-body">
+                                <h5 class="card-title">Radiografia</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Saber +</a>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    {/*                   
                     <div className="col-lg-4 col-sm-12 text-center p-3">
                         <h3 id="services">Estetica</h3>
                         <img src={imagen7} class="d-block w-100" alt="..." />
@@ -50,7 +114,7 @@ class Services extends Component {
                         <h3 id="services">Radiofrafia</h3>
                         <img src={imagen4} class="d-block w-100" alt="..." />
 
-                    </div>
+                    </div> */}
 
                 </div>
 
