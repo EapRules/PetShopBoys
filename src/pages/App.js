50eb5733c5ea9a6e22693c98fc0b0d9aa97440f0
Lucas <lukaq66@gmail.com>
2020-01-17 14:37:39 -0300

    footer

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../Components/Header/Navbar/Navbar';
 import Slider from '../Components/Slider/Slider';
 import Market from '../Components/Market/Market';
+import Footer from '../Components/Footer/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar></Navbar>
       <Slider></Slider>
       <Market />
+      <Footer></Footer>
     </div>
   );
 }
