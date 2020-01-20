4065af59102c2e06176e8d841bc0d7f5bd58792c
Hy-An Exequiel <nathan.hyan2048@gmail.com>
2020-01-20 14:40:30 -0300

    Login finished

--- src/Components/Auth/Login.js
+++ src/Components/Auth/Login.js
@@ -1,24 +1,37 @@
 import React, { Component, Fragment } from 'react'
 
 export default class Login extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+
+    handleChange = obj => {
+        let { name, value } = obj.target
+
+        this.setState({ [name]: value })
+    }
+
+    onSubmit = obj => {
+obj.preventDefault()
+console.log(this.state)
+}
     render() {
         return (
-
-            <form>
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />>
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                    <label for="exampleInputEmail1">Dirección de correo</label>
+                    <input type="email" className="form-control" placeholder="Ingrese su mail" name='username' value={this.state.username} onChange={this.handleChange} />
+                    <small id="emailHelp" className="form-text text-muted">No la usaremos para enviarte spam. Lo prometemos! 🤞.</small>
                 </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+                <div className="form-group">
+                    <label for="exampleInputPassword1">Contraseña</label>
+                    <input type="password" className="form-control" placeholder="Ingrese su contraseña" name='password' value={this.state.password} onChange={this.handleChange} />
                 </div>
-                <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" className="btn btn-success w-100 mt-4">Ingresar</button>
             </form>
 
         )
