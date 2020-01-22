098ca78898182c559d64f264b4b14feab9e59389
Lucas <lukaq66@gmail.com>
2020-01-22 14:11:34 -0300

    listado-producto

--- src/pages/App.js
+++ src/pages/App.js
@@ -4,6 +4,8 @@ import Slider from '../Components/Slider/Slider';
 import Services from '../Components/Services/Services';
 import Market from '../Components/Market/Market';
 import Footer from '../Components/Footer/Footer';
+import ProductList from '../Components/Admin-Page/Product-list/Product-list';
+
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
       <Services></Services>
       <Market />
       <Footer></Footer>
+      <ProductList></ProductList>
     </div>
   );
 }
