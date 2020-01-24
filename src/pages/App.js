60c69ee5f8060a8478188276249668f6ff96a96e
Hy-An Exequiel <nathan.hyan2048@gmail.com>
2020-01-24 10:22:08 -0300

    Fixing the router issue

--- src/pages/App.js
+++ src/pages/App.js
@@ -5,16 +5,19 @@ import Services from '../Components/Services/Services';
 import Market from '../Components/Market/Market';
 import Footer from '../Components/Footer/Footer';
 import ProductList from '../Components/Admin-Page/Product-list/Product-list';
+import { BrowserRouter as Router, Route, withRouter } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <Navbar />
       <Slider />
       <Services />
       <Market />
       <Footer />
+      </Router>
     </div>
   );
 }
