4438c07290cbfb49fe4deda528c8508325731d4b
franco flores <franco210789@gmail.com>
2020-01-24 13:10:58 -0300

    sidebar y route

--- src/pages/App.js
+++ src/pages/App.js
@@ -5,16 +5,37 @@ import Services from '../Components/Services/Services';
 import Market from '../Components/Market/Market';
 import Footer from '../Components/Footer/Footer';
 import ProductList from '../Components/Admin-Page/Product-list/Product-list';
+import Sidebar from '../Components/Admin-Page/Sidebar/Sidebar';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Slider />
-      <Services />
-      <Market />
-      <Footer />
+     
+     <Router>
+        <main>
+          <Navbar />
+            <Switch>
+            <Route exact path="/">
+              <Slider/>
+              <Services />
+              <Market />
+              </Route>
+              <Route exact path="/admin" component={Sidebar}>
+              </Route>
+              <Route exact path="/admin/productos" component={Sidebar,ProductList}>
+                
+              </Route>
+          </Switch>
+          <Footer/>
+        </main> 
+      </Router>
     </div>
   );
 }
