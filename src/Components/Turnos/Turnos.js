c94784e8fee5c3f4d0d59aede1d5b99783a1396d
EapRules <enzoapoliti@gmail.com>
2020-02-09 15:39:03 -0300

    add post turns

--- src/Components/Turnos/Turnos.js
+++ src/Components/Turnos/Turnos.js
@@ -1,6 +1,55 @@
 import React, { Component } from 'react'
+import Swal from 'sweetalert2'
 
 export default class Turnos extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            ownerName: '',
+            petName: '',
+            date: '',
+        }
+    }
+    handleChange = obj => {
+        let { name, value } = obj.target
+        this.setState({ [name]: value })
+    }
+    onSubmit = async obj => {
+        console.log("btn funciona")
+        if (obj) {
+            obj.preventDefault();
+        }
+        const response = await fetch(
+            "https://rolling-pet-shop.herokuapp.com/turns",
+            {
+                method: "POST",
+                body: JSON.stringify(this.state),
+                headers: {
+                    "Content-Type": "application/json",
+                    "authorization": `Bearer ${localStorage.getItem("token")}`
+                }
+            }
+        );
+        const res = await response.json();
+        if (!res.errors) {
+            Swal.fire({
+                icon: 'success',
+                title: 'Turno creado Exitosamente',
+                text: `${res.date}`,
+                showConfirmButton: true
+            })
+        }
+        else {
+            Swal.fire({
+                icon: 'error',
+                title: `Complete todos los campos`,
+                showConfirmButton: false,
+                timer: 1500
+            })
+        }
+
+    };
+
     render() {
         return (
             <div>
@@ -13,68 +62,61 @@ export default class Turnos extends Component {
                     </div>
                     <div className="col-6">
                         <h2 className="text-center">Horarios preferencia</h2>
-                        <form>
+                        <form onSubmit={this.onSubmit}>
                             <div className="form-row">
-                                <div className="form-group col-md-6">
-                                    <label htmlFor="inputEmail4">Desde</label>
-                                    <input type="email" className="form-control" id="inputEmail4"></input>
-                                </div>
-                                <div className="form-group col-md-6">
-                                    <label htmlFor="inputPassword4">Hasta</label>
-                                    <input type="password" className="form-control" id="inputPassword4"></input>
+                                {/* <div className="form-group col-md-6 disabled">
+                                    <label htmlFor="DesdeDate">Desde</label>
+                                    <input type="date" className="form-control disabled" name="DesdeDate" value={this.state.username} onChange={this.handleChange} ></input>
+                                </div> */}
+                                <div className="form-group col-md-12">
+                                    <label htmlFor="HastaDate">Fecha</label>
+                                    <input type="date" className="form-control" name="date" value={this.state.date} onChange={this.handleChange}></input>
                                 </div>
                             </div>
-                        </form>
-
+                            {/*  
                         <h2 className="text-center">Servicios</h2>
                         <div className="row">
                             <div className="col-6">
                                 <div className="form-group ">
                                     <label htmlFor="inputState">Opciones</label>
-                                    <select id="inputState" className="form-control">
+                                    <select name="inputState" className="form-control">
                                         <option selected></option>
                                         <option>...</option>
                                     </select>
                                 </div>
-
                             </div>
-
                             <div className="col-6">
-                                <label htmlFor="inputEmail4">Lista de servicios</label>
-                                <textarea class="form-control" id="message-text"></textarea>
-
+                                <label htmlFor="message-text">Lista de servicios</label>
+                                <textarea class="form-control" name="message-text"></textarea>
                             </div>
-
                         </div>
-                        <h2 className="text-center">Datos cliente</h2>
-                        <form>
+                        */}
+                            <h2 className="text-center">Datos cliente</h2>
                             <div className="form-row">
                                 <div className="form-group col-md-6">
-                                    <label htmlFor="inputEmail4">Nombre y Apellido</label>
-                                    <input type="email" className="form-control" id="inputEmail4"></input>
+                                    <label htmlFor="textNombre">Nombre y Apellido</label>
+                                    <input type="text" className="form-control" name="ownerName" value={this.state.ownerName} onChange={this.handleChange}></input>
                                 </div>
                                 <div className="form-group col-md-6">
-                                    <label htmlFor="inputPassword4">Telefono</label>
-                                    <input type="password" className="form-control" id="inputPassword4"></input>
+                                    <label htmlFor="inputPassword4">Nombre Mascota</label>
+                                    <input type="text" className="form-control" name="petName" value={this.state.petName} onChange={this.handleChange}></input>
                                 </div>
                             </div>
-                        </form>
-                        <form>
                             <div className="form-row">
                                 <div className="form-group col-md-6">
-                                    <label htmlFor="inputEmail4">Email</label>
-                                    <input type="email" className="form-control" id="inputEmail4"></input>
+                                    <label htmlFor="Email">Telefono</label>
+                                    <input type="tel" className="form-control" name="tel"></input>
                                 </div>
                                 <div className="form-group col-6">
                                     <label htmlFor="inputState">Forma de pago</label>
-                                    <select id="inputState" className="form-control ">
+                                    <select name="inputState" className="form-control ">
                                         <option selected>Efectivo</option>
                                         <option>...</option>
                                     </select>
                                 </div>
                             </div>
+                            <button type="submit" className="btn btn-success w-100 mt-4">Solicitar Turno</button>
                         </form>
-
                     </div>
                 </div>
             </div>
