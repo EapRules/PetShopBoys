92115d7e7460dca46d635fa019a7058b61da6a21
walter celiz <walterceliz93@gmail.com>
2020-02-19 15:38:44 -0300

    Footer fixed

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 
 export default class Footer extends Component {
@@ -26,22 +27,22 @@ export default class Footer extends Component {
                 <ul className="list-unstyled ">
                   <li>
                     <p>
-                      <a className="" href="#!">Inicio</a>
+                      <Link className="" to="/" >Inicio</Link>
                     </p>
                   </li>
                   <li>
                     <p>
-                      <a className="" href="#!">Tienda</a>
+                    <Link className="" to="/Tienda" >Tienda</Link>
                     </p>
                   </li>
                   <li>
                     <p>
-                      <a className="" href="#!">Turno </a>
+                    <Link className="" to="/Turnos" >Turno</Link>
                     </p>
                   </li>
                   <li>
                     <p>
-                      <a className="" href="#!">Consultas</a>
+                    <Link className="" to="/Consultas" >Consulta</Link>
                     </p>
                   </li>
                 </ul>
