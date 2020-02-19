78b5f0759f0dd09108c7ad26712dbd93106b5c9e
EapRules <enzoapoliti@gmail.com>
2020-02-19 00:23:07 -0300

    add new selector for dates nad hours

--- src/Components/Turnos/Turnos.js
+++ src/Components/Turnos/Turnos.js
@@ -2,48 +2,86 @@ import React, { Component } from 'react'
 import Swal from 'sweetalert2'
 import { BeatLoader } from "react-spinners";
 import Loader from '../shared/Loader';
+import moment from 'moment'
+
+
+
 
 export default class Turnos extends Component {
+
     constructor(props) {
         super(props)
         this.state = {
+            hour: '',
+            day: '',
             ownerName: '',
             petName: '',
             date: '',
-            loading: false
+            DataTurnos: [],
+            loading: false,
+            startDate: new Date()
         }
     }
     componentDidMount() {
         let token = localStorage.getItem("token");
-        this.setState({ isLoggedIn: token ? true : false })
+        this.setState({ isLoggedIn: token ? true : false });
+
     }
-    handleChange = obj => {
-        let { name, value } = obj.target
-        this.setState({ [name]: value })
-    }
-    onSubmit = async obj => {
-        this.setState({ loading: true })
-        if (obj) {
-            obj.preventDefault();
-        }
-        const response = await fetch(
-            "https://rolling-pet-shop.herokuapp.com/turns",
+
+    getDataByday() {
+        let selectDate = this.state.day
+        fetch(
+            `https://rolling-pet-shop.herokuapp.com/turns?date=${selectDate}`,
             {
-                method: "POST",
-                body: JSON.stringify(this.state),
+                method: "Get",
                 headers: {
                     "Content-Type": "application/json",
                     "authorization": `Bearer ${localStorage.getItem("token")}`
                 }
             }
+        ).then(res => res.json())
+            .then(res => this.setState({ DataTurnos: res.result }))
+    }
+    handleChange = obj => {
+        this.getDataByday()
+        let { name, value } = obj.target
+        this.setState({ [name]: value })
+        console.log(this.state);
+
+    }
+
+    onSubmit = async obj => {
+
+        this.setState({ loading: true })
+        if (obj) {
+            obj.preventDefault();
+        }
+
+        const response = await fetch(
+            "https://rolling-pet-shop.herokuapp.com/turns",
+            {
+                method: "POST",
+                body: JSON.stringify(
+                    {
+                        ownerName: `${this.state.ownerName}`,
+                        petName: `${this.state.petName}`,
+                        date: `${this.state.day}T${this.state.hour}`
+                    }
+                ),
+                headers: {
+                    "Content-Type": "application/json",
+                    "authorization": `Bearer ${localStorage.getItem("token")}`
+                }
+            }
+
         );
         const res = await response.json();
         this.setState({ loading: false })
         if (!res.errors) {
             Swal.fire({
                 icon: 'success',
-                title: 'Turno creado Exitosamente',
-                text: `${res.date}`,
+                title: `${moment(res.date).format('[El] DD [de] MMMM, [a las] h:mm a')}`,
+                text: `Turno creado con exito`,
                 showConfirmButton: true
             })
         }
@@ -55,7 +93,7 @@ export default class Turnos extends Component {
                 timer: 1500
             })
         }
-
+        this.getDataByday()
     };
 
     render() {
@@ -67,44 +105,124 @@ export default class Turnos extends Component {
                     color={"green"}
                     loading={this.state.loading}
                 />
-                <h1 className="text-center m-5 main-title">Turnos</h1>
+                <h1 className="text-center m-5">Turnos</h1>
                 <div className="row m-5">
-                    <div className="col-5">
+                    <div className="col-lg-6 col-xs-12 text-center" >
                         <div>
-                            <h2 className="text-center">  Calendario</h2>
+                            <h2 className="text-center">Horarios preferencia</h2>
+                        </div>
+                        <div className="form-group text-center">
+                            <label htmlFor="HastaDate">Fecha</label>
+                            <input type="date" className="form-control text-center" name="day" value={this.state.day} onChange={this.handleChange}></input>
+                        </div>
+                        <label htmlFor="HastaDate">Horario</label>
+                        <div class="row justify-content-around">
+
+                            <div className="col-lg-6 col-xs-12" >
+                                <table className="table table-borderless table-sm table-white text-center">
+                                    <thead class="table-success">
+                                        <tr >
+                                            <th colspan="2">Turnos mañana</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {/* {this.state.DataTurnos.map((item, i) => (
+                                        <tr>
+                                            <th key={i}>{i}</th>
+                                            <td>{item.petName}</td>
+                                            <td>{item.ownerName}</td>
+                                            <td>{item.date}</td>
+                                        </tr>
+                                    ))} */}
+                                        <tr>
+                                            <td >9:00 am</td>
+                                            <td><button type="button" class="btn btn-primary btn-sm" name="hour" value="12:00" onClick={this.handleChange}>Agendar</button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>9:30 am</td>
+                                            <td><button type="button" class="btn btn-primary btn-sm" name="hour" value="12:30" onClick={this.handleChange}>Agendar</button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>10:00 am</td>
+                                            <td>
+                                                <button type="button" class="btn btn-primary btn-sm" name="hour" value="13:00" onClick={this.handleChange}>Agendar</button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>10:30 am</td>
+                                            <td><button type="button" class="btn btn-primary btn-sm" name="hour" value="13:30" onClick={this.handleChange}>Agendar</button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>11:00 am</td>
+                                            <td><button type="button" class="btn btn-primary btn-sm" name="hour" value="14:00" onClick={this.handleChange}>Agendar</button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>11:30 am</td>
+                                            <td><button type="button" class="btn btn-primary btn-sm" name="hour" value="14:30" onClick={this.handleChange}>Agendar</button>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div className="col-lg-6 col-xs-12" >
+                                <table className="table table-borderless table-sm table-white text-center">
+                                    <thead class="table-success">
+                                        <tr >
+                                            <th colspan="2">Turnos tarde</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {/* {this.state.DataTurnos.map((item, i) => (
+                                        <tr>
+                                            <th key={i}>{i}</th>
+                                            <td>{item.petName}</td>
+                                            <td>{item.ownerName}</td>
+                                            <td>{item.date}</td>
+                                        </tr>
+                                    ))} */}
+                                        <tr>
+                                            <td>6:00 pm</td>
+                                            <td><button type="button" class="btn btn-primary btn-sm" name="hour" value="21:00" onClick={this.handleChange}>Agendar</button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>6:30 pm</td>
+                                            <td><button type="button" class="btn btn-primary btn-sm" name="hour" value="21:30" onClick={this.handleChange}>Agendar</button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>7:00 pm</td>
+                                            <td>
+                                                <button type="button" class="btn btn-primary btn-sm" name="hour" value="22:00" onClick={this.handleChange}>Agendar</button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>7:30 pm</td>
+                                            <td><button type="button" class="btn btn-primary btn-sm" name="hour" value="22:30" onClick={this.handleChange}>Agendar</button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>8:00 pm</td>
+                                            <td><button type="button" class="btn btn-primary btn-sm" name="hour" value="23:00" onClick={this.handleChange}>Agendar</button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>8:30 pm</td>
+                                            <td><button type="button" class="btn btn-primary btn-sm" name="hour" value="23:30" onClick={this.handleChange}>Agendar</button>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
-                    <div className="col-6">
-                        <h2 className="text-center">Horarios preferencia</h2>
+                    <div className="col-lg-6 col-xs-12 ">
+
                         <form onSubmit={this.onSubmit}>
-                            <div className="form-row">
-                                {/* <div className="form-group col-md-6 disabled">
-                                    <label htmlFor="DesdeDate">Desde</label>
-                                    <input type="date" className="form-control disabled" name="DesdeDate" value={this.state.username} onChange={this.handleChange} ></input>
-                                </div> */}
-                                <div className="form-group col-md-12">
-                                    <label htmlFor="HastaDate">Fecha</label>
-                                    <input type="date" className="form-control" name="date" value={this.state.date} onChange={this.handleChange}></input>
-                                </div>
-                            </div>
-                            {/*  
-                        <h2 className="text-center">Servicios</h2>
-                        <div className="row">
-                            <div className="col-6">
-                                <div className="form-group ">
-                                    <label htmlFor="inputState">Opciones</label>
-                                    <select name="inputState" className="form-control">
-                                        <option selected></option>
-                                        <option>...</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div className="col-6">
-                                <label htmlFor="message-text">Lista de servicios</label>
-                                <textarea class="form-control" name="message-text"></textarea>
-                            </div>
-                        </div>
-                        */}
                             <h2 className="text-center">Datos cliente</h2>
                             <div className="form-row">
                                 <div className="form-group col-md-6">
@@ -116,21 +234,17 @@ export default class Turnos extends Component {
                                     <input type="text" className="form-control" name="petName" value={this.state.petName} onChange={this.handleChange}></input>
                                 </div>
                             </div>
-                            <div className="form-row">
-                                <div className="form-group col-md-6">
-                                    <label htmlFor="Email">Telefono</label>
-                                    <input type="tel" className="form-control" name="tel"></input>
-                                </div>
-                                <div className="form-group col-6">
-                                    <label htmlFor="inputState">Forma de pago</label>
-                                    <select name="inputState" className="form-control ">
-                                        <option defaultValue>Efectivo</option>
-                                        <option>...</option>
-                                    </select>
+                            <div className="form-row text-center justify-content-center">
+
+
+                                <div>
+                                    <div className="form-group textarea-turno">
+                                        <label>Escriba su consulta aquí</label>
+                                        <textarea name="message_html" className="form-control" rows="9" cols="5" ></textarea>
+
+                                    </div>
                                 </div>
                             </div>
-
-
                             <button type={this.state.isLoggedIn ? "submit" : "button"} className={this.state.isLoggedIn ? 'btn w-100 btn-success' : 'btn w-100 btn-danger'}>{this.state.isLoggedIn ? 'Solicitar Turno' : 'Debe iniciar sesion para Solicitar un Turno'}</button>
                         </form>
                     </div>
